ec63cc2b1ebdf1123edec83b170c6655834c3cce
Billy F <willfira717@gmail.com>
2020-11-07 17:15:01 -0500

    Added has_secure_password to Player model

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,6 +4,29 @@ class PlayersController < ApplicationController
         @players = Player.all
     end 
 
+    def show
+        @player = Player.find(params[:id])
+    end 
+
+    def new
+        @player = Player.new 
+    end 
+
+    def create
+        @player = Player.create(player_params)
+        if @player.valid?
+            redirect_to players_path
+        else 
+            flash[:alert] = "Could not create Player. Please try again."
+            render new_player_path
+        end 
+    end 
 
 
 end
+
+private
+
+def player_params
+    params.require(:player).permit(:name, :password)
+end 
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+    has_secure_password
+
     has_many :characters
     has_many :adventures, through: :characters
 end
